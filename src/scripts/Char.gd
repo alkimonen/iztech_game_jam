7715cb58eeb1e886ba0be3d2540d090b5203fa53
Alkım Önen <alkimonen@gmail.com>
2021-03-27 15:22:13 +0300

    Update char.gd

--- src/scripts/Char.gd
+++ src/scripts/Char.gd
@@ -12,8 +12,8 @@ onready var move_direction = Vector2()
 onready var animated_sprite = $AnimatedSprite
 
 var locations = []
+var loc_index = -1
 var is_jumping = false
-var loc_index = 0
 
 func _ready():
 	add_new_location( Vector2(20, 20))
@@ -22,9 +22,8 @@ func _ready():
 	add_new_location( Vector2(160, 50))
 	add_new_location( Vector2(180, 50))
 	
-	self.position = locations[loc_index]
-	
-	pass
+	_calculate_direction(0)
+	_turn()
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") && !is_jumping:
@@ -32,19 +31,40 @@ func _physics_process(delta):
 			loc_index += 1
 		else:
 			loc_index = 0
+		_jump_to(locations[loc_index])
 		
-		_calculate_direction(locations[loc_index])
-		is_jumping = true
-		animated_sprite.play("jump")
-	
 	if is_jumping && _close_to(locations[loc_index]):
-		is_jumping = false
-		self.position.x = locations[loc_index].x
-		self.position.y = locations[loc_index].y
-		animated_sprite.play("land")
-		
+		_land()
+	_apply_movement()
+
+func _calculate_direction(index):
+	if index < locations.size():
+		move_direction.x = locations[index].x - self.position.x
+		move_direction.y = locations[index].y - self.position.y
+		move_direction = move_direction.normalized()
+	else:
+		move_direction.x = 1
+		move_direction.y = 0
+
+func _turn():
+	self.rotation = move_direction.angle()
+	
+func _close_to(next_pos):
+	if abs(next_pos.x - self.position.x) <= CLOSENESS_DISTANCE && abs(next_pos.y - self.position.y) <= CLOSENESS_DISTANCE:
+		return true
+	return false
+
+func _jump_to(next_pos):
+	is_jumping = true
+	animated_sprite.play("jump")
+
+func _land():
+	is_jumping = false
+	self.position = locations[loc_index]
+	animated_sprite.play("land")
+
+func _apply_movement():
 	if is_jumping:
-		self.rotation = move_direction.angle()
 		speed = max( speed + DECELERATION, MIN_SPEED)
 		motion.x = speed * move_direction.x
 		motion.y = speed * move_direction.y
@@ -52,30 +72,13 @@ func _physics_process(delta):
 		speed = MAX_SPEED
 		motion.x = 0
 		motion.y = 0
-	
 	move_and_slide(motion)
-	
-
-func _calculate_direction(next_pos):
-	move_direction.x = next_pos.x - self.position.x
-	move_direction.y = next_pos.y - self.position.y
-	move_direction = move_direction.normalized()
-
-func _close_to(next_pos):
-	#print( abs(next_pos.x - self.position.x), " ", abs(next_pos.y - self.position.y))
-	if abs(next_pos.x - self.position.x) <= CLOSENESS_DISTANCE && abs(next_pos.y - self.position.y) <= CLOSENESS_DISTANCE:
-		return true
-	return false
-
-func _move_to(location):
-	pass
 
 func add_new_location(new_pos):
 	locations.append(new_pos)
 
-func _apply_animation():
-	pass
-
 func _on_AnimatedSprite_animation_finished():
 	if animated_sprite.get_animation() == "land":
 		animated_sprite.play("idle")
+		_calculate_direction(loc_index+1)
+		_turn()
