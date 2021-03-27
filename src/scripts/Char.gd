2963a8b75b86f92b02526a4d07ad27d3609c6dce
Alkım Önen <alkimonen@gmail.com>
2021-03-27 16:08:09 +0300

    Add movement mechanics and tilemap

--- src/scripts/Char.gd
+++ src/scripts/Char.gd
@@ -14,26 +14,23 @@ onready var animated_sprite = $AnimatedSprite
 var locations = []
 var loc_index = -1
 var is_jumping = false
+var landed = true
 
 func _ready():
-	add_new_location( Vector2(20, 20))
-	add_new_location( Vector2(50, 100))
-	add_new_location( Vector2(100, 70))
-	add_new_location( Vector2(160, 50))
-	add_new_location( Vector2(180, 50))
-	
-	_calculate_direction(0)
-	_turn()
+	pass
+	#add_new_location( Vector2(20, 20))
+	#add_new_location( Vector2(50, 100))
+	#add_new_location( Vector2(100, 70))
+	#add_new_location( Vector2(160, 50))
+	#add_new_location( Vector2(180, 50))
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("ui_accept") && !is_jumping:
+	if Input.is_action_just_pressed("ui_accept") && landed:
 		if loc_index < locations.size()-1:
 			loc_index += 1
-		else:
-			loc_index = 0
 		_jump_to(locations[loc_index])
 		
-	if is_jumping && _close_to(locations[loc_index]):
+	if !landed && _close_to(locations[loc_index]):
 		_land()
 	_apply_movement()
 
@@ -56,6 +53,7 @@ func _close_to(next_pos):
 
 func _jump_to(next_pos):
 	is_jumping = true
+	landed = false
 	animated_sprite.play("jump")
 
 func _land():
@@ -76,9 +74,14 @@ func _apply_movement():
 
 func add_new_location(new_pos):
 	locations.append(new_pos)
+	if loc_index == -1:
+		_calculate_direction(0)
+		_turn()
+	print( "added")
 
 func _on_AnimatedSprite_animation_finished():
 	if animated_sprite.get_animation() == "land":
 		animated_sprite.play("idle")
 		_calculate_direction(loc_index+1)
 		_turn()
+		landed = true
